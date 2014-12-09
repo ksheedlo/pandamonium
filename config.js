d9f8607da3ee411e1f188fbacd39a0a0f42c9bf6
Ken Sheedlo <ken.sheedlo@rackspace.com>
2014-12-09 15:12:31 -0800

    feat(entities): script to add random entities

---
+++ config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var fs = require('graceful-fs'),
+  path = require('path');
+
+var filename = process.env.PM_SETTINGS || path.join(process.env.HOME, '.pmrc'),
+  configFile;
+
+try {
+  configFile = fs.readFileSync(filename, 'utf8');
+  configFile = JSON.parse(configFile);
+} catch (err) {
+  if (err.code === 'ENOENT') {
+    configFile = {};
+  } else {
+    console.error('Error loading config file:', filename);
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  RACKSPACE_USERNAME: configFile.username || '',
+  RACKSPACE_API_KEY: configFile.api_key || ''
+};
