8fe72ec16b2af8bd705554149f521ca36d72a65a
Ken Sheedlo <ken.sheedlo@rackspace.com>
2014-12-25 16:35:12 -0600

    test(Notification): add tests for Notification.create

--- test/notifications.js
+++ test/notifications.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var expect = require('chai').expect,
+var Chance = require('chance'),
+  expect = require('chai').expect,
   rewire = require('rewire'),
   ntList = rewire('../lib/notifications/list'),
-  maasQueryCalls;
+  ntCreate = rewire('../lib/notifications/create'),
+  maasQueryCalls,
+  maasRequestCalls;
 
-ntList.__set__('maas', {
+var MaasMock = {
   query: function () {
     var args = Array.prototype.slice.call(arguments, 0),
       cb = args[1];
@@ -14,12 +17,25 @@ ntList.__set__('maas', {
     process.nextTick(function () {
       cb(null, [{ id: 'fakeNotificationExample' }]);
     });
+  },
+  request: function () {
+    var args = Array.prototype.slice.call(arguments, 0),
+      cb = args[1];
+
+    maasRequestCalls.push({ args: args });
+    process.nextTick(function () {
+      cb(null, { headers: { 'x-object-id': 'nt123456' } });
+    });
   }
-});
+};
+
+ntCreate.__set__('maas', MaasMock);
+ntList.__set__('maas', MaasMock);
 
 describe('Notification', function () {
   beforeEach(function () {
     maasQueryCalls = [];
+    maasRequestCalls = [];
   });
 
   describe('.list', function () {
@@ -33,6 +49,61 @@ describe('Notification', function () {
           path: '/notifications'
         });
         expect(res).to.exist();
+        done();
+      });
+    });
+  });
+
+  describe('.create', function () {
+    var chance, oldDateNow;
+
+    before(function () {
+      oldDateNow = Date.now;
+      Date.now = function () {
+        return 1419544462490;
+      };
+    });
+
+    beforeEach(function () {
+      chance = new Chance(1);
+    });
+
+    after(function () {
+      Date.now = oldDateNow;
+    });
+
+    it('makes the request to maas', function (done) {
+      ntCreate.create(chance, function (err) {
+        var maasRequestArg;
+
+        if (err) {
+          return done(err);
+        }
+
+        maasRequestArg = maasRequestCalls[0].args[0];
+        expect(maasRequestArg.path).to.equal('/notifications');
+        expect(maasRequestArg.method).to.equal('POST');
+        expect(maasRequestArg.body.label).to.equal('obaudde @ko');
+        done();
+      });
+    });
+
+    it('creates a random notification', function (done) {
+      ntCreate.create(chance, function (err, notification) {
+        if (err) {
+          return done(err);
+        }
+        expect(notification).to.eql({
+          label: "obaudde @ko",
+          type: "sms",
+          details: {
+            phone_number: "+1395843652"
+          },
+          metadata: {
+            pandamonium: 1419544462490
+          },
+          id: "nt123456"
+        });
         done();
       });
     });
