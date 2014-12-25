dab8bbe4e972c4a866c2754c1943f4fd832cab32
Ken Sheedlo <ken.sheedlo@rackspace.com>
2014-12-25 16:05:19 -0600

    test(Entity): add tests for Entity.create

--- test/entities.js
+++ test/entities.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var expect = require('chai').expect,
+var Chance = require('chance'),
+  expect = require('chai').expect,
   rewire = require('rewire'),
+  entitiesCreate = rewire('../lib/entities/create'),
   entitiesList = rewire('../lib/entities/list'),
-  maasQueryCalls;
+  maasQueryCalls,
+  maasRequestCalls;
 
-entitiesList.__set__('maas', {
+var MaasMock = {
   query: function () {
     var args = Array.prototype.slice.call(arguments, 0),
       cb = args[1];
@@ -14,12 +17,25 @@ entitiesList.__set__('maas', {
     process.nextTick(function () {
       cb(null, [{ id: 'fakeEntityExample' }]);
     });
+  },
+  request: function () {
+    var args = Array.prototype.slice.call(arguments, 0),
+      cb = args[1];
+
+    maasRequestCalls.push({ args: args });
+    process.nextTick(function () {
+      cb(null, { headers: { 'x-object-id': 'en123456' } });
+    });
   }
-});
+};
+
+entitiesCreate.__set__('maas', MaasMock);
+entitiesList.__set__('maas', MaasMock);
 
 describe('Entity', function () {
   beforeEach(function () {
     maasQueryCalls = [];
+    maasRequestCalls = [];
   });
 
   describe('.list', function () {
@@ -33,6 +49,61 @@ describe('Entity', function () {
           path: '/entities'
         });
         expect(res).to.exist();
+        done();
+      });
+    });
+  });
+
+  describe('.create', function () {
+    var chance, oldDateNow;
+
+    before(function () {
+      oldDateNow = Date.now;
+      Date.now = function () {
+        return 1419544462490;
+      };
+    });
+
+    beforeEach(function () {
+      chance = new Chance(1);
+    });
+
+    after(function () {
+      Date.now = oldDateNow;
+    });
+
+    it('makes the request to maas', function (done) {
+      entitiesCreate.create(chance, function (err) {
+        var requestArgs;
+
+        if (err) {
+          return done(err);
+        }
+
+        requestArgs = maasRequestCalls[0].args[0];
+        expect(requestArgs.path).to.equal('/entities');
+        expect(requestArgs.method).to.equal('POST');
+        expect(requestArgs.body.label).to.equal('wubju de ko rukvi');
+        done();
+      });
+    });
+
+    it('creates a random entity', function (done) {
+      entitiesCreate.create(chance, function (err, entity) {
+        if (err) {
+          return done(err);
+        }
+        expect(entity).to.eql({
+          label: "wubju de ko rukvi",
+          ip_addresses: {
+            internet0_v6: "37e3:08ae:6786:2f3c:cbfc:51b8:eded:1d04",
+            internet0_v4: "43.15.224.171"
+          },
+          metadata: {
+            pandamonium: 1419544462490
+          },
+          id: "en123456"
+        });
         done();
       });
     });
