5aded891767e1b7b92c1264d846bc0c17a0b0c5d
Ken Sheedlo <ken.sheedlo@rackspace.com>
2015-01-05 00:12:27 -0800

    test(Check): add tests for Check.create

--- test/checks.js
+++ test/checks.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var expect = require('chai').expect,
+  Chance = require('chance'),
   rewire = require('rewire'),
+  checksCreate = rewire('../lib/checks/create'),
   checksList = rewire('../lib/checks/list'),
-  maasQueryCalls;
+  maasQueryCalls,
+  maasRequestCalls;
 
-checksList.__set__('maas', {
+var MaasMock = {
   query: function () {
     var args = Array.prototype.slice.call(arguments, 0),
       cb = args[1];
@@ -14,12 +17,25 @@ checksList.__set__('maas', {
     process.nextTick(function () {
       cb(null, [{ id: 'fakeCheckExample' }]);
     });
+  },
+  request: function () {
+    var args = Array.prototype.slice.call(arguments, 0),
+      cb = args[1];
+
+    maasRequestCalls.push({ args: args });
+    process.nextTick(function () {
+      cb(null, { headers: { 'x-object-id': 'ch101C0D' } });
+    });
   }
-});
+};
+
+checksCreate.__set__('maas', MaasMock);
+checksList.__set__('maas', MaasMock);
 
 describe('Check', function () {
   beforeEach(function () {
     maasQueryCalls = [];
+    maasRequestCalls = [];
   });
 
   describe('.list', function () {
@@ -33,6 +49,64 @@ describe('Check', function () {
           path: '/entities/en123FOO/checks'
         });
         expect(res).to.exist();
+        done();
+      });
+    });
+  });
+
+  describe('.create', function () {
+    var chance, oldDateNow;
+
+    before(function () {
+      oldDateNow = Date.now;
+      Date.now = function () {
+        return 1419544462490;
+      };
+    });
+
+    beforeEach(function () {
+      chance = new Chance(1);
+    });
+
+    after(function () {
+      Date.now = oldDateNow;
+    });
+
+    it('makes the request to maas', function (done) {
+      checksCreate.create(chance, { entityId: 'en123456' }, function (err) {
+        var requestArgs;
+
+        if (err) {
+          return done(err);
+        }
+
+        requestArgs = maasRequestCalls[0].args[0];
+        expect(requestArgs.path).to.equal('/entities/en123456/checks');
+        expect(requestArgs.method).to.equal('POST');
+        expect(requestArgs.body.label).to.equal('wubju de ko');
+        done();
+      });
+    });
+
+    it('creates a random check', function (done) {
+      checksCreate.create(chance, {
+        entityId: 'en123456'
+      }, function (err, check) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(check).to.eql({
+          type: "agent.filesystem",
+          label: "wubju de ko",
+          metadata: {
+            pandamonium: "1419544462490"
+          },
+          details: {
+            target: "/var"
+          },
+          id: "ch101C0D"
+        });
         done();
       });
     });
